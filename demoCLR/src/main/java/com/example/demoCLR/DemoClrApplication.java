507e41665a8e2cdb5ad1abd4c1e471e5b5e9bc01
harshalhabib03 <harshalhabib2002@gmail.com>
2025-03-27 10:12:24 +0530

    Command Line Runner

---
+++ demoCLR/src/main/java/com/example/demoCLR/DemoClrApplication.java
@@ -0,0 +1,15 @@
+package com.example.demoCLR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
+
+@Component
+@SpringBootApplication
+public class DemoClrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoClrApplication.class, args);
+	}
+
+}
---
+++ demoCLR/src/test/java/com/example/demoCLR/DemoClrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoCLR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoClrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
