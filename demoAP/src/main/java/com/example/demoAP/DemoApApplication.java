8d11193994d7e5e8ad353407de53fe8f70e15afc
harshalhabib03 <harshalhabib2002@gmail.com>
2025-03-27 10:10:39 +0530

    Demo Application

---
+++ demoAP/src/main/java/com/example/demoAP/DemoApApplication.java
@@ -0,0 +1,15 @@
+package com.example.demoAP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
+
+@Component
+@SpringBootApplication
+public class DemoApApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApApplication.class, args);
+	}
+
+}
---
+++ demoAP/src/test/java/com/example/demoAP/DemoApApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoAP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
